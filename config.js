6f68f0cc14457a4b9fba6849b1a48dd3edebf03e
nittyjee <nittyjee@gmail.com>
2025-04-02 01:38:48 -0500

    Token reference to config file

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 // Do not EVER give this token out, or make it available online. Make sure this file is in .gitignore
 const MAPBOX_TOKEN = 'pk.eyJ1Ijoibml0dHlqZWUiLCJhIjoiY204emc5cWZnMDQ0ZzJycTJhaHdnNzZ2cCJ9.Vy9Ki1fqwWLVVqjQJefSxQ';
+
+//mapny MAPBOX_TOKEN. Never ever put this online or give it without permission:
+//pk.eyJ1IjoibWFwbnkiLCJhIjoiY200OW03ZGh2MGJyMzJrcTEydW4wMGh2eSJ9.eJnHIk7wriv-Hp02T7mT3g
